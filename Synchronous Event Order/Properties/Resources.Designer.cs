a30a3f5410eeb98391177801f311992e682f1a10
John Raesly <john.raesly@gmail.com>
2020-06-01 18:58:47 -0600

    Update CDS logos

--- Synchronous Event Order/Properties/Resources.Designer.cs
+++ Synchronous Event Order/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Synchronous_Event_Order.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -63,9 +63,39 @@ namespace Synchronous_Event_Order.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap CDS {
+            get {
+                object obj = ResourceManager.GetObject("CDS", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap tsbClose_Image {
+            get {
+                object obj = ResourceManager.GetObject("tsbClose_Image", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap tsbLoadEvents_Image {
             get {
                 object obj = ResourceManager.GetObject("tsbLoadEvents.Image", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap tsbUpdate_Image {
+            get {
+                object obj = ResourceManager.GetObject("tsbUpdate_Image", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
